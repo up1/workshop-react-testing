b6386edd71e590e59013bd7e79a8b3d9d9a47048
somkiat <somkiat.p@gmail.com>
2018-03-23 20:22:04 +0700

    Test case to fillin number and click plus with enzyme

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 
 class Calculator extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      result: null,
+    }
+    this.elements = {}
+    this.onPlus = this.onPlus.bind(this);
+  }
+
   render() {
+    const { result } = this.state
     return (
         <form>
-          <input type="number" defaultValue={ 0 }/>
-          <input type="number" defaultValue={ 0 }/>
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>/</button>
-          <p className='result'/>
+          <input type="number" defaultValue={ 0 } ref={ (el) => this.elements.firstValue = el }/>
+          <input type="number" defaultValue={ 0 } ref={ (el) => this.elements.secondValue = el }/>
+          <button type="button" className='plus' onClick={ this.onPlus }>+</button>
+          <button type="button">-</button>
+          <button type="button">*</button>
+          <button type="button">/</button>
+          <p className='result'>{ result }</p>
         </form>
     );
+  }
+
+  onPlus() {
+    const firstValue = parseInt(this.elements.firstValue.value, 10)
+    const secondValue = parseInt(this.elements.secondValue.value, 10)
+    const resultPlus = this.plus(firstValue, secondValue)
+    this.setState({
+      result: resultPlus,
+    });
   }
 
   plus(firstValue, secondValue) {
--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Calculator from './Calculator'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 it('Should display calculator page correctly', () => {
   const calculator = shallow(<Calculator />)
@@ -10,6 +10,18 @@ it('Should display calculator page correctly', () => {
   expect(form.find('input').length).toBe(2)
   expect(form.find('button').length).toBe(4)
   expect(form.find('p.result').length).toBe(1)
+});
+
+it('Should display result for plus correctly', () => {
+  const calculator = mount(<Calculator />)
+  // กำหนดค่า 1 และ 2
+  const form = calculator.find('form')
+  form.childAt(0).instance().value = 1;
+  form.childAt(1).instance().value = 2;
+  // กดปุ่มบวก
+  form.find('button.plus').simulate('click')
+  // ผลการบวกต้องเป็น 3
+  expect(form.find('p.result').text()).toEqual('3')
 });
 
 it('Should return the correct from plus', () => {
